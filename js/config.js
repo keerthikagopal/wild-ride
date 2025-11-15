91707a5055506c8a788b2263d0555e4e2acb5a10
keerthikagopal <keerthikaggopal@gmail.com>
2025-11-15 14:14:33 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,8 @@ window._config = {
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://vpdv0dwyeh.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://s3ezfv6j0l.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
