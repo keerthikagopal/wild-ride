bd85e8ff06e276793ce62624417c57b900d7fe34
keerthikagopal <keerthikaggopal@gmail.com>
2025-11-17 11:27:22 +0530

    Update AWS Cognito and API configuration

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_wGyjnyk2V', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '27h879iqu2c9j3rgtt7j81ljg6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_pvg4RGBVD', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '72akshpjtppo2ls9aqjrbpkgvb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://s3ezfv6j0l.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://wrxd20q378.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
